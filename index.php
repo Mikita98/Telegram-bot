572a7d585073ad8e0133b8d57142c65310439667
Mikita <memesnotwar@gmail.com>
2018-05-26 13:00:52 +0300

    Mafia

--- index.php
+++ index.php
@@ -8,40 +8,39 @@
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-    $keyboard = [["Последние статьи"],["Картинка"],["5 сек"]]; //Клавиатура
+    $keyboard = [["Фамилия имя отчество?"],["Кем являетесь?"],["Где именно?"],["Кличка?"],["Воуу блааааа"]]; //Клавиатура
 
     if($text){
          if ($text == "/start") {
-            $reply = "Добро пожаловать в бота!";
+            $reply = "Погнали";
             $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-        }elseif ($text == "/help") {
-            $reply = "Информация с помощью.";
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-        }elseif ($text == "Картинка") {
-            $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-            $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-        }elseif ($text == "5 сек") 
+        }elseif ($text == "Фамилия имя отчество?") 
 		{
-            //$url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
-            //$telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-			//$w1 = new EvTimer(2, 0, function () 
-			//{
-				$reply = "Таймер сработал";	
-				$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
-			//});
+            $reply = "Зубенко Михаил Петрович";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
         }
-		elseif ($text == "Последние статьи") {
-            $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
-            foreach ($html->channel->item as $item) {
-	     $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
-        	}
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
-        }else{
-        	$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
-        	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
+		elseif ($text == "Кем являетесь?") 
+		{
+            $reply = "Вор в законе";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
         }
+		elseif ($text == "Где именно?") 
+		{
+            $reply = "Шумиловский городок";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+		}
+		elseif ($text == "Кличка?") 
+		{
+            $reply = "Мафиозник";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+		}
+		elseif ($text == "Воуу блааааа") 
+		{
+            $reply = "https://www.youtube.com/watch?v=0V79ANwcmQw";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply  ]);
+		}
     }else{
-    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "По русски говори э." ]);
     }
 ?>
