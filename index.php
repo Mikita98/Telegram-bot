54e3f08d893fc043ce450b62fde5d126d2307cf2
Mikita <memesnotwar@gmail.com>
2018-05-26 12:41:36 +0300

    check timer

--- index.php
+++ index.php
@@ -21,14 +21,17 @@
         }elseif ($text == "Картинка") {
             $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
             $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
-        }elseif ($text == "5 сек") {
+        }elseif ($text == "5 сек") 
+		{
             //$url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
             //$telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
-			$w1 = new EvTimer(2, 0, function () {
-			$reply = "таймер сработал";	
-			$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
-				});
-        }elseif ($text == "Последние статьи") {
+			//$w1 = new EvTimer(2, 0, function () 
+			//{
+				$reply = "Таймер сработал";	
+				$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
+			//});
+        }
+		elseif ($text == "Последние статьи") {
             $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
             foreach ($html->channel->item as $item) {
 	     $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
