2b77380f5c87b312a8a0519495be059ffae5ed82
Mikita <memesnotwar@gmail.com>
2018-05-30 10:31:37 +0300

    change mysql

--- db_connect.php
+++ db_connect.php
@@ -7,11 +7,11 @@ $username = "root";
 $databasename = "revcom_base";
 
 global $db;
-$db = mysql_connect($host,$username,$password) or die("error: Failed_connect_database");
+$db = mysqli_connect($host,$username,$password) or die("error: Failed_connect_database");
 
-mysql_select_db($databasename, $db) or die("error:Database not selected witch mysql_select_db");
+mysqli_select_db($databasename, $db) or die("error:Database not selected witch mysql_select_db");
 
-mysql_query('SET NAMES utf8',$db);
-mysql_query('SET CHARACTER SET utf8',$db );
-mysql_query('SET COLLATION_CONNECTION="utf8_general_ci"',$db ); 
+mysqli_query('SET NAMES utf8',$db);
+mysqli_query('SET CHARACTER SET utf8',$db );
+mysqli_query('SET COLLATION_CONNECTION="utf8_general_ci"',$db );
 setlocale(LC_ALL,"ru_RU.UTF8");
--- users.php
+++ users.php
@@ -2,8 +2,8 @@
 
 function make_user($name,$chat_id){
 	global $db;
-	$name = mysql_real_escape_string($name);
-	$chat_id = mysql_real_escape_string($chat_id);
+	$name = mysqli_real_escape_string($name);
+	$chat_id = mysqli_real_escape_string($chat_id);
 	$query = "insert into `users`(name,chat_id) values('{$name}','{$chat_id}')";
-	mysql_query($query,$db) or die("пользователя создать не удалось");
+	mysqli_query($query,$db) or die("пользователя создать не удалось");
 }
